0440854f62f95cfeca3e429e7a4a89b7c7f00f2c
maBeghtel <m_a_beghtel@hotmail.com>
2024-07-08 13:59:32 -0500

    Added the assets folder, with image, and setup server to provide those files.

--- main.go
+++ main.go
@@ -13,18 +13,24 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
 
 	godotenv.Load()
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Port is not found in the environment")
 	}
-
 	fmt.Println("Server listening to port: 8080")
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(portString, mux)
+
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("index.html"))
 	tmpl.Execute(w, nil)
 }
+
+/*func handleAssets(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseGlob("logo.png"))
+	tmpl.Execute(w, nil)
+}*/
